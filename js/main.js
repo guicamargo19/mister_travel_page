0c4eaa855bfb7e34f238068b3dc6f9f899a0c9df
camargo <gui.ferreira.camargo@gmail.com>
2024-04-18 18:14:45 -0300

    Alterado CSS e README.md

--- js/main.js
+++ js/main.js
@@ -1,39 +1,42 @@
 $(document).ready(function() {
 
     $("#carrossel-imagens").slick({
-        autoplay: true
+        autoplay: true,
+        arrows: false,
+        speed: 600,
     })
 
     $("#carrossel-imagens2").slick({
-        autoplay: true
+        autoplay: true,
+        arrows: false,
+        speed: 400
     })
 
     $("#carrossel-imagens3").slick({
-        autoplay: true
+        autoplay: true,
+        arrows: false,
+        speed: 900
     })
 
     $("#carrossel-imagens4").slick({
-        autoplay: true
+        autoplay: true,
+        arrows: false,
+        speed: 400
     })
 
     $("#carrossel-imagens5").slick({
-        autoplay: true
+        autoplay: true,
+        arrows: false,
+        speed: 600
     })
 
     $("#carrossel-imagens6").slick({
-        autoplay: true
-    })
-
-    $('.menu').click(function(){
-        $('nav').slideToggle();
-    })
-
-    $('.container li>a').click(function(){
-        $('nav').slideToggle();
+        autoplay: true,
+        arrows: false,
+        speed: 800
     })
 
     $('#telefone').mask('(00) 00000-0000')
-    $('#cpf').mask('000.000.000-00')
     $('#cep').mask('00000-000')
 
     $('form').validate({
@@ -50,9 +53,6 @@ $(document).ready(function() {
             cep: {
                 required: true,
             },
-            cpf: {
-                required: true,
-            },
             viagemInteresse: {
                 required: true,
             },
@@ -65,7 +65,6 @@ $(document).ready(function() {
             nome: 'Por favor, digite seu nome',
             telefone: 'Por favor, digite um telefone',
             cep: 'Por favor, digite seu CEP',
-            cpf: 'Por favor, digite seu CPF',
             telefone: 'Por favor, digite seu telefone',
             viagemInteresse: 'Por favor, informe qual destino tem interesse',
         },
@@ -76,7 +75,6 @@ $(document).ready(function() {
             $('#telefone').val('')
             $('#email').val('')
             $('#cep').val('')
-            $('#cpf').val('')
             $('#viagemInteresse').val('')
             $('#mensagem').val('')
         }
@@ -91,7 +89,27 @@ $(document).ready(function() {
 
         $('html').animate({
             scrollTop: destino.offset().top
-        }, 1000)
+        }, 200)
 
     })
 })
+
+window.addEventListener('scroll', function() {
+    var scrollToTopBtn = document.getElementById('scrollToTopBtn');
+  
+    // Mostra o botão quando o usuário rola para baixo 200px da parte superior da página
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      scrollToTopBtn.style.display = 'block';
+    } else {
+      scrollToTopBtn.style.display = 'none';
+    }
+  });
+  
+  function scrollToTop() {
+    // Scroll suave para o topo da página
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+  
